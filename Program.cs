e55a9cdc7585352f253cdf280b492fd7550eef51
keshavprakash98 <keshavprakash98@gmail.com>
2021-10-27 21:24:34 +0530

    day5

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Year");
+            int year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0))
+            {
+                Console.WriteLine(year + " is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+        }
+    }
+}
